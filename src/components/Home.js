2f6e0211d515de649c7f1482d624dd362514a28f
the-shivam <shivamthakurofficial@gmail.com>
2021-10-26 18:59:28 +0530

    first

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+export default function Home() {
+    const [searchtext, setText] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (searchtext.length === 0) {
+            alert("Enter text to search")
+            return;
+        }
+        console.log(searchtext);
+        setText("");
+    }
+    return (
+        <div style={{ backgroundColor: "#120f1a", height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <form onSubmit={handleSubmit}>
+                <input id="search-box" type="text" value={searchtext} onChange={(e) => setText(e.target.value)} placeholder="Enter you text" />
+                <input style={{ backgroundColor: "black", color: "white", borderColor: "white" }} type='submit' />
+            </form>
+        </div>
+    )
+}
